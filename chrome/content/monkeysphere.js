3c45034d4ada8d155204843478afe3af7de4bebf
Jameson Rollins <jrollins@finestructure.net>
2010-01-13 12:59:57 -0500

    add copyright to monkeysphere.js file

--- chrome/content/monkeysphere.js
+++ chrome/content/monkeysphere.js
@@ -1,3 +1,19 @@
+// Monkeysphere XUL extension
+// Copyright © 2010 Jameson Rollins <jrollins@finestructure.net>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 // Monkeysphere global namespace
 var monkeysphere = {
 
@@ -319,8 +335,7 @@ var monkeysphere = {
     var client = new XMLHttpRequest();
     client.open("POST", agent_url, true);
 
-    //monkeysphere.log("query", "sending query: " + query);
-    monkeysphere.log("query", "sending query:");
+    // set headers
     client.setRequestHeader("Content-Type", "application/json");
     client.setRequestHeader("Content-Length", query.length);
     client.setRequestHeader("Connection", "close");
@@ -330,6 +345,7 @@ var monkeysphere = {
       monkeysphere.onAgentStateChange(client, browser, cert);
     };
 
+    monkeysphere.log("query", "sending query:");
     client.send(query);
     monkeysphere.log("query", "query sent");
   },
