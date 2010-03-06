8bda4c2f40e87d76b7c0efd3977f6b882b814428
mike castleman <m@mlcastle.net>
2010-03-06 14:10:48 -0500

    bugfix

--- chrome/content/monkeysphere.js
+++ chrome/content/monkeysphere.js
@@ -242,7 +242,7 @@ var monkeysphere = {
 ////////////////////////////////////////////////////////////
   
   getDefaultStatusText: function(state) {
-    var key = monkeysphere.states.state ? ("status" + state) : "xulError";
+    var key = (typeof monkeysphere.states.state !== 'undefined') ? ("status" + state) : "xulError";
     return monkeysphere.messages.getString(key);
   },
 
